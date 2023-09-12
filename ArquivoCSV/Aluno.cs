7c16d8201c27f43eaa9e950f5edbd25949f6c24f
Billpzt <billpiazzetta@gmail.com>
2023-09-12 12:02:57 -0300

    First commit

---
+++ ArquivoCSV/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquivoCSV {
+    public class Aluno {
+        public Aluno() {
+        }
+        public Aluno(string nome, double n1, double n2, double n3) {
+            Nome = nome;
+            N1 = n1;
+            N2 = n2;
+            N3 = n3;
+        }
+        public string Nome { get; set; }
+        public double N1 { get; set; }
+        public double N2 { get; set; }
+        public double N3 { get; set; }
+
+        public override string ToString() {
+            return Nome + " " + N1 + " " + N2 + " " + N3;
+        }
+    }
+}
---
+++ ArquivoCSV/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ArquivoCSV {
+    internal class Program {
+        static void Main(string[] args) {
+            LerArquivo();
+        }
+
+        private static void LerArquivo() {
+            const String nomeArquivo = "notas.csv";
+            const String diretorio = @"C:\Users\william.piazzetta\Documents\BackEnd\FundamentosCsharp";
+
+            String caminho = Path.Combine(diretorio, nomeArquivo);
+            if (!File.Exists(caminho)) {
+                Console.WriteLine("Erro: Arquivo não existe");
+            }
+            try {
+                using (var arquivo = new StreamReader(caminho)) { // Abertura do arquivo
+                    String linha = arquivo.ReadLine();
+                    while (linha != null) { // Testa o final do arquivo - EOF
+                        String[] campos = linha.Split(";");
+                        Aluno aluno = new Aluno(campos[0], double.Parse(campos[1]), double.Parse(campos[2]), double.Parse(campos[3]));
+                        Console.WriteLine(aluno);
+                        linha = arquivo.ReadLine();
+                    }
+                }
+            }
+            catch {
+                Console.WriteLine("Erro!");
+            }
+        }
+    }
+}
